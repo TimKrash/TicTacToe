d67b5650f2a9b046adec7e1d7da9b60b91d6da24
Tim Krashevsky <tim.krashevsky@gmail.com>
2022-09-30 12:41:12 -0500

    init computer logic

--- script.js
+++ script.js
@@ -22,13 +22,19 @@ const gameController = (() => {
   }
   const getPlayers = () => players;
 
-  const switchTurn = () => {
+  const sleep = ms => new Promise(r => setTimeout(r, ms));
+  const switchTurn = async () => {
     prevTurn = currTurn;
 
     if (currTurn === players[0]) {
       currTurn = players[1];
     } else {
       currTurn = players[0];
+    }
+
+    if (currTurn.getType() === "Computer") {
+      await sleep(200);
+      handleComputerMove(currTurn);
     }
   }
 
@@ -69,23 +75,22 @@ const gameController = (() => {
   // End game logic
   const isGameOver = (row, col) => {
     let board = gameBoard.getBoard();
-    let prevTurnUser = prevTurn;
-    let prevTurnMarker = prevTurnUser.getMarker();
+    let currTurnMarker = currTurn.getMarker();
 
-    if (board[row].every(x => x === prevTurnMarker)) {
+    if (board[row].every(x => x === currTurnMarker)) {
       return true;
     }
 
     for (let i = 0; i < board.length; i++) {
-      if (board[i][col] == prevTurnMarker && i != board.length - 1) {
+      if (board[i][col] == currTurnMarker && i != board.length - 1) {
         continue
-      } else if (board[i][col] == prevTurnMarker) {
+      } else if (board[i][col] == currTurnMarker) {
         return true;
       }
       break;
     }
 
-    if (onDiag(row, col, prevTurnMarker)) {
+    if (onDiag(row, col, currTurnMarker)) {
       return true;
     }
 
@@ -94,9 +99,6 @@ const gameController = (() => {
 
   const isValidMove = (row, col) => {
     if (gameBoard.getSquare(row, col) == "") {
-
-      // Switch user turn now
-      switchTurn();
       return true;
     }
     return false;
@@ -104,8 +106,27 @@ const gameController = (() => {
 
   const getCurrTurn = () => currTurn;
 
+  const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  const handleComputerMove = (currUser) => {
+    let row = getRandomInt(0,2);
+    let col = getRandomInt(0,2);
+
+    while (!isValidMove(row, col)) {
+      row = getRandomInt(0,2);
+      col = getRandomInt(0,2);
+    }
+
+    currUser.mark(row, col);
+    displayController.addComputerMove(currUser, row, col);
+  }
+
   return {
-    isValidMove, getCurrTurn, isGameOver, createPlayer, getPlayers
+    isValidMove, getCurrTurn, isGameOver, createPlayer, getPlayers, switchTurn
   };
 
 })()
@@ -115,6 +136,34 @@ const displayController = (() => {
   let playerTwo;
   let startContainer = document.querySelector(".start-container");
   let board = document.querySelector(".board");
+
+  const handleNewMove = (currUser, target, row, col) => {
+    let userMarker = currUser.getMarker();
+    currUser.mark(row, col);
+    if (gameBoard.getSquare(row, col) == userMarker) {
+      target.textContent = userMarker;
+
+      if (gameController.isGameOver(row, col)) {
+        let gameOver = document.querySelector(".game-over");
+        let winner = gameOver.querySelector(".winner");
+        let playAgain = gameOver.querySelector("button");
+        playAgain.addEventListener('click', (e) => {
+          window.location.reload();
+        });
+
+        board.style.display = "none";
+
+        winner.textContent = "Game Over! " + currUser.getName() + " won!";
+        gameOver.style.display = "flex";
+      }
+      gameController.switchTurn();
+    }
+  }
+
+  const addComputerMove = (currUser, row, col) => {
+    let target = document.querySelector(`div[id="${row}-${col}"]`);
+    handleNewMove(currUser, target, row, col);
+  }
 
   const handleStartPlay = (e) => {
     e.preventDefault();
@@ -143,25 +192,7 @@ const displayController = (() => {
       return;
     }
 
-    let userMarker = currUser.getMarker();
-    currUser.mark(row, col);
-    if (gameBoard.getSquare(row, col) == userMarker) {
-      target.textContent = userMarker;
-
-      if (gameController.isGameOver(row, col)) {
-        let gameOver = document.querySelector(".game-over");
-        let winner = gameOver.querySelector(".winner");
-        let playAgain = gameOver.querySelector("button");
-        playAgain.addEventListener('click', (e) => {
-          window.location.reload();
-        });
-
-        board.style.display = "none";
-
-        winner.textContent = "Game Over! " + currUser.getName() + " won!";
-        gameOver.style.display = "flex";
-      }
-    }
+    handleNewMove(currUser, target, row, col);
   }
 
   let squares = document.querySelectorAll(".square");
@@ -171,6 +202,8 @@ const displayController = (() => {
 
   let form = document.querySelector("form");
   form.addEventListener('submit', handleStartPlay);
+
+  return { addComputerMove  }
 })()
 
 const gameBoard = (() => {
